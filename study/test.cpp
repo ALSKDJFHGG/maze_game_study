7af2cac522c8fc336287656c59af0dc4bcd4db81
Yang <2294288093@qq.com>
2025-01-16 16:08:56 +0800

    用缓冲优化了屏幕闪烁问题
    支持宽字符并优化控制台显示和游戏逻辑
    
    将字符数组 `s` 从 `char` 类型更改为 `wchar_t` 类型，以支持宽字符。
    在 `print` 函数中，使用 `CreateConsoleScreenBuffer` 和 `WriteConsoleOutputCharacter` 函数来替代 `cout`，以便在控制台中显示宽字符。
    在 `check` 和 `win_check` 函数中，将参数类型从 `char` 更改为 `wchar_t`。
    在 `win_check` 函数中，增加了对 `@` 字符的检查，如果遇到 `@` 字符，增加分数 `score`。
    在 `move_` 函数中，增加了对 `win_check` 的调用，如果游戏胜利，输出分数并退出程序。
    删除了 `main` 函数中的 `win_check` 调用，因为已经在 `move_` 函数中处理。
    删除了 `system("cls")` 调用，改为使用 `SetConsoleActiveScreenBuffer` 来刷新控制台。

--- study/test.cpp
+++ study/test.cpp
@@ -6,43 +6,64 @@ using namespace std;
 #define HEIGHT 6
 #define WIDTH 10
 
+int score = 0;
 
-char s[HEIGHT][WIDTH] = {
-	"#########",
-	"#  #    #",
-	"# #   # #",
-	"# # # # #",
-	"#   #  @#",
-	"#########"
+wchar_t s[HEIGHT][WIDTH] = {
+	L"#########",
+	L"#  #    #",
+	L"#@#@@ # #",
+	L"# # # # #",
+	L"#   #  @#",
+	L"#########"
 };
 
-void print(char s[HEIGHT][WIDTH], int h, int w) {
+void print(wchar_t s[HEIGHT][WIDTH], int h, int w) {
+
+	//创建新的缓冲区
+	HANDLE hOutput = CreateConsoleScreenBuffer(
+		GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_READ | FILE_SHARE_WRITE,
+		NULL,
+		CONSOLE_TEXTMODE_BUFFER,
+		NULL
+	);
+
+
+	//隐藏缓冲区的光标
+	CONSOLE_CURSOR_INFO cci;
+	cci.bVisible = 0;
+	cci.dwSize = 1;
+	SetConsoleCursorInfo(hOutput, &cci);
+
+	//缓冲处理显示
+	DWORD bytes = 0;
+	COORD coord = { 0, 0 };
+
 	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w; j++) {
-			cout << s[i][j];
-		}
-		cout << endl;
+		coord.Y = i;
+		WriteConsoleOutputCharacter(hOutput, (LPCWSTR)s[i], w, coord, &bytes);
 	}
+	SetConsoleActiveScreenBuffer(hOutput);
 }
 
-bool check(char s[HEIGHT][WIDTH], int x, int y) {
+bool check(wchar_t s[HEIGHT][WIDTH], int x, int y) {
 	if (s[x][y] == '#') {
 		return false;
 	}
 	return true;
 }
 
-bool win_check(char s[HEIGHT][WIDTH], int x, int y) {
-	/*if (s[x][y] == '#') {
-		return false;
-	}*/
+bool win_check(wchar_t s[HEIGHT][WIDTH], int x, int y) {
+	if (s[x][y] == '@') {
+		score++;
+	}
 	if (x == 4 && y == 7) {
 		return true;
 	}
 	return false;
 }
 
-void move_(char s[HEIGHT][WIDTH], int* x, int* y) {
+void move_(wchar_t s[HEIGHT][WIDTH], int* x, int* y) {
 	if (_kbhit()) {
 		char c = _getch();
 		if (c == 'w') {
@@ -70,11 +91,16 @@ void move_(char s[HEIGHT][WIDTH], int* x, int* y) {
 			}
 		}
 	}
+
+	if (win_check(s, *x, *y)) {
+		cout << "Your score: " << score << endl;
+		exit(0);
+	}
+
 	s[*x][*y] = '@';
 	print(s, HEIGHT, WIDTH);
 	Sleep(5);
 	s[*x][*y] = ' ';
-	system("cls");
 }
 
 int x = 1, y = 1;
@@ -83,10 +109,6 @@ int x = 1, y = 1;
 int main() {
 	while (1) {
 		move_(s, &x, &y);
-		if (win_check(s, x, y)) {
-			cout << "You win!" << endl;
-			exit(0);
-		}
 	}
 	return 0;
 }
